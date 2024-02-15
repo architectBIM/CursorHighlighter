84476c25540b5a27944e6d874ee05535794dd6e2
Maxim Stepannikov <mstep@bimplanet.org>
2024-02-16 00:09:12 +0600

    Delete MVC

--- model.py
+++
@@ -1,4 +0,0 @@
-class AppModel:
-    def __init__(self):
-        self.cursor_color = "red"
-        self.click_highlight_color = "blue"
--- view.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-class AppView:
-    def __init__(self, master):
-        self.master = master
-        self.initialize_ui()
-
-    def initialize_ui(self):
-        self.master.title("Cursor Highlighter")
